3d4920148681c93c4fd54c3bed0b359a94a5abbd
Andreas von Studnitz <avs@integer-net.de>
2013-08-26 16:51:41 +0200

    Added support for billsafe installment payment

--- src/app/code/community/IntegerNet/MagentoLocalizedEbay/Model/Observer.php
+++ src/app/code/community/IntegerNet/MagentoLocalizedEbay/Model/Observer.php
@@ -23,6 +23,8 @@ class IntegerNet_MagentoLocalizedEbay_Model_Observer
         $this->_setConfigData('payment/billsafe/sandbox', $params['billsafe_sandbox']);
         $this->_setConfigData('payment/billsafe/merchant_id', $params['billsafe_merchant_id']);
         $this->_setConfigData('payment/billsafe/merchant_license', $params['billsafe_merchant_license']);
+        $this->_setConfigData('payment/billsafe_installment/active', $params['billsafe_installment_active']);
+        $this->_setConfigData('payment/billsafe_installment/sandbox', $params['billsafe_installment_sandbox']);
     }
 
     /**
