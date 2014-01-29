c482e347d7870ef802448cb62776e979c41121e3
Andreas von Studnitz <avs@integer-net.de>
2014-01-29 11:55:35 +0100

    Retrieve HTML Data from web or file

---
+++ src/app/code/community/IntegerNet/MagentoLocalizedEbay/Block/Adminhtml/Page/Head.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * integer_net Magento Module
+ *
+ * @category   IntegerNet
+ * @package    IntegerNet_MagentoLocalizedEbay
+ * @copyright  Copyright (c) 2013 integer_net GmbH (http://www.integer-net.de/)
+ * @author     Andreas von Studnitz <avs@integer-net.de>
+ */
+class IntegerNet_MagentoLocalizedEbay_Block_Adminhtml_Page_Head extends Mage_Adminhtml_Block_Page_Head
+{
+    public function getCssJsHtml()
+    {
+        $html = parent::getCssJsHtml();
+
+        if ($this->getRequest()->getControllerName() == 'magentoLocalized' && $this->getRequest()->getActionName() == 'form') {
+            $externalHtmlData = Mage::helper('magento_localized_ebay')->getHtmlData();
+
+            if (isset($externalHtmlData['css'])) {
+                $html .= '<link rel="stylesheet" type="text/css" href="' . $externalHtmlData['css'] . '" />' . "\n";
+            }
+
+            if (isset($externalHtmlData['js'])) {
+                $html .= '<script type="text/javascript" src="' . $externalHtmlData['js'] . '"></script>' . "\n";
+            }
+        }
+
+        return $html;
+    }
+}
--- src/app/code/community/IntegerNet/MagentoLocalizedEbay/Helper/Data.php
+++ src/app/code/community/IntegerNet/MagentoLocalizedEbay/Helper/Data.php
@@ -8,4 +8,67 @@
  * @author     Andreas von Studnitz <avs@integer-net.de>
  */
 class IntegerNet_MagentoLocalizedEbay_Helper_Data extends Mage_Core_Helper_Abstract
-{}
+{
+    protected $_htmlData = null;
+
+    /**
+     * @return array
+     */
+    public function getHtmlData()
+    {
+        if (is_null($this->_htmlData)) {
+            $htmlData = $this->_getHtmlDataFromWeb();
+            if (sizeof($htmlData)) {
+                $this->_htmlData = $htmlData;
+                Mage::log('web');
+            } else {
+                $this->_htmlData = $this->_getHtmlDataFromFile();
+                Mage::log('file');
+            }
+        }
+
+        return $this->_htmlData;
+    }
+
+    /**
+     * @return string
+     */
+    protected function _getHtmlDataUrl()
+    {
+        return Mage::getStoreConfig('magento_localized/htmldata_url');
+    }
+
+    /**
+     * @return array
+     */
+    protected function _getHtmlDataFromWeb()
+    {
+        $url = $this->_getHtmlDataUrl();
+
+        try {
+            $request = new Zend_Http_Client($url);
+            $response = $request->request();
+            return Zend_Json::decode($response->getBody());
+        } catch (Exception $e) {
+            Mage::logException($e);
+            return array();
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function _getHtmlDataFromFile()
+    {
+        $filename = Mage::getModuleDir('etc', 'IntegerNet_MagentoLocalizedEbay') . DS . 'htmldata.json';
+        if (is_readable($filename)) {
+            $fileContents = file_get_contents($filename);
+            try {
+                return Zend_Json::decode($fileContents);
+            } catch (Exception $e) {
+                Mage::logException($e);
+                return array();
+            }
+        }
+    }
+}
