45ffd54a9d688481221abc4b34703c4603307a77
Andreas von Studnitz <avs@integer-net.de>
2014-02-07 09:28:13 +0100

    Changed text for paypal configuration

--- src/app/code/community/IntegerNet/MagentoLocalizedEbay/Block/Form.php
+++ src/app/code/community/IntegerNet/MagentoLocalizedEbay/Block/Form.php
@@ -34,11 +34,6 @@ class IntegerNet_MagentoLocalizedEbay_Block_Form extends Mage_Adminhtml_Block_Wi
         return Mage::helper('adminhtml')->getUrl('adminhtml/magentolocalizedebay/paypalPopup');
     }
 
-    public function getPayPalConfigUrl()
-    {
-        return Mage::helper('adminhtml')->getUrl('adminhtml/magentolocalizedebay/paypal');
-    }
-
     public function getPayPalApiUrl()
     {
         return 'https://www.paypal.com/cgi-bin/customerprofileweb?cmd=_profile-api-access';
